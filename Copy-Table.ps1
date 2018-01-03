a6a8d7b95d945a2ce36663edf82075c5ee3e9e9e
Henry Lawrance <hlawrance@otterbein.edu>
2018-01-03 12:27:52 -0500

    it works?

--- Copy-Table.ps1
+++ Copy-Table.ps1
@@ -1,4 +1,84 @@
-function Copy-Table ()
-{
+Import-Module ./modules/invoke-oracmd.psm1
+function Global:Copy-Table {
+    [CmdletBinding()]
+    Param(
+        [parameter(Mandatory=$true)][String] $SourceTableName,
+        [parameter(Mandatory=$true)][String] $DestinationTableName,
+        [parameter(Mandatory=$false)][String] $DestinationDatabaseName = "OUDATA",
+        [parameter(Mandatory=$false)][String] $DestinationServerName = "ocsql2014",
+        [parameter(Mandatory=$false)][Switch] $OverwriteExisting = $false,
+        [parameter(Mandatory=$false)][Switch] $RunOutput = $false,
+        [parameter(Mandatory=$false)][String] $OutputFile = "output.sql"
+
+    )
+    if(Test-Path $OutputFile) {
+        Remove-Item $OutputFile
+    }
+    $Columns = (Invoke-Oracmd -Query "select COLUMN_NAME,DATA_TYPE,DATA_LENGTH,NULLABLE from all_tab_columns where table_name = '$($SourceTableName)'" -Prod -OutputDataSet).Tables
+    if($Columns.COLUMN_NAME.count -eq 0) { "Table Name does not exist in Oracle (Check Capitalization)"; Return }
+    $OraData = Invoke-Oracmd -Query "select * from $($SourceTableName) where rownum <= 500" -Prod -OutputDataSet
+    $TableCheck = Invoke-Sqlcmd -Query "select * from sys.tables where name = '$($DestinationTableName)'" -ServerInstance $DestinationServerName -Database $DestinationDatabaseName
+    if($TableCheck.name.count -eq 0) {   
+        $CreateQuery = "CREATE TABLE [dbo].[$DestinationTableName](`n"
+        Foreach($Column in $Columns) {
+            $data_type = switch($Column.DATA_TYPE) {
+                "NUMBER" { "VARCHAR" }
+                "VARCHAR2" { "VARCHAR" }
+                "DATE" { "VARCHAR" }
+                default { $Column.DATA_TYPE }
+            }
+            $data_length = switch($Column.DATA_TYPE) {
+                "DATE" { 50 }
+                default { $Column.DATA_LENGTH }
+            }
+            $null_type = switch($Column.NULLABLE) {
+                "N" { "NOT NULL" }
+                default { "NULL" }
+            }
+
+            $CreateQuery += "`t[$($Column.COLUMN_NAME)] [$($data_type)] ($($data_length)) $null_type"
+            if(([array]::IndexOf($Columns.COLUMN_NAME, $Column.COLUMN_NAME))+1 -lt $Columns.COLUMN_NAME.Count) {
+                $CreateQuery += ",`n"
+            } else {
+                $CreateQuery += "`n"
+            }
+        }
+        $CreateQuery += ")"
+
+        $CreateQuery | out-file -enc ascii $OutputFile -Append
+    } else {
+        if($OverwriteExisting.IsPresent) {
+            $DeleteQuery = "TRUNCATE TABLE $($DestinationTableName)"
+            $DeleteQuery | out-file -enc ascii $OutputFile -Append
+        } else {
+            "Table already exists. Use -OverwriteExisting switch to override"; Return
+        }
+    }
+
+    
+    ForEach($Line in $OraData.Tables) {
+        $InsertQuery = "INSERT INTO $($DestinationTableName) ("
+        $InsertQuery += $Columns.COLUMN_NAME -join ","
+        $InsertQuery += ")`n`tVALUES ("
+        foreach($Column_Name in $Columns.COLUMN_NAME) {
+            $value = $Line[$Column_Name]
+            $b_value = switch($value.GetType().Name) {
+                "String" { $value.replace("'","''") }
+                default { $value }
+            }
+            $InsertQuery += "`'$b_value`'"
+            if(([array]::IndexOf($Columns.COLUMN_NAME, $Column_Name))+1 -lt $Columns.COLUMN_NAME.Count) {
+                $InsertQuery += ","
+            }
+        }
+        $InsertQuery += ")"
+        $InsertQuery | out-file -enc ascii $OutputFile -Append
+        
+    }
+
+    switch($RunOutput.IsPresent) {
+        $true { Invoke-Sqlcmd -InputFile .\output.sql -ServerInstance $DestinationServerName -Database $DestinationDatabaseName | out-file -FilePath .\log.rpt }
+    }
     
 }
+#Copy-Table -SourceTableName GZRADUS -DestinationTableName GZRADUS -DestinationDatabaseName OUTestData -DestinationServerName ocsql2014
---
+++ modules/invoke-oracmd.psm1
@@ -0,0 +1,33 @@
+﻿Add-type -Path "C:\oracle\instantclient_10_2\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+function Invoke-Oracmd {
+    Param(
+    [parameter(Mandatory=$true,Position=0)][String] $Query,
+    [parameter(Mandatory=$false)][switch] $Test,
+    [parameter(Mandatory=$false)][switch] $Prod,
+    [parameter(Mandatory=$false)][switch] $OutputDataSet
+    )
+    
+    if($Test.IsPresent) {
+        $tns="Data Source= (DESCRIPTION =(ADDRESS =(PROTOCOL = TCP)(HOST = 205.133.226.85)(PORT = 8002))(CONNECT_DATA =(SERVICE_NAME = TEST.ottu.edu)));User Id=ouautomation;Password=fhpD0vO8;"
+    } elseif ($Prod.IsPresent) {
+        $tns="Data Source= (DESCRIPTION =(ADDRESS =(PROTOCOL = TCP)(HOST = 205.133.226.84)(PORT = 8002))(CONNECT_DATA =(SERVICE_NAME = PROD.ottu.edu)));User Id=ouautomation;Password=fhpD0vO8;"
+    } else {
+        Write-Error "Prod or Test not specified. Use -Test or -Prod."
+    }
+    $con = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($tns)
+	$con.open()
+	$command=$con.CreateCommand()
+	$command.CommandText=$Query 
+	$da=New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter($command)
+	$BanUsers = @()
+	$BanUsers = New-Object System.Data.DataSet 
+	$da.fill($BanUsers)
+	$con.Close()
+    if($OutputDataSet.IsPresent) {
+        $BanUsers
+    } else {
+	    $BanUsers.Tables[0]
+    }
+}
+
+Export-ModuleMember -Function *
