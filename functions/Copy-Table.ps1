26592e46f7c0ae309d11498d59762ba531f08f64
Henry Lawrance <hlawrance@otterbein.edu>
2018-08-16 16:55:24 -0400

    Update Copy-Table.ps1

--- functions/Copy-Table.ps1
+++ functions/Copy-Table.ps1
@@ -10,16 +10,16 @@ function Copy-Table {
     Use -OutputFile "$(Get-date -f 'MMddyy')_update.sql" to generate a daily SQL file
     Use -LogFile to define a specific error log location
 .EXAMPLE
-    Copy-Table -SourceTableName GZRADUS -DestinationTableName GZRADUS -DestinationDatabaseName OUTestData -DestinationServerName ocsql2014 -RunOutput -OverwriteExisting
+    Copy-Table -SourceTableName SOURCE -DestinationTableName DEST -DestinationDatabaseName TestData -DestinationServerName sql2014 -RunOutput -OverwriteExisting
         - Copy table, overwriting any existing data, execute on SQL server
 .EXAMPLE        
-    Copy-Table -SourceTableName GZRADUS -DestinationTableName TEMPTABLE -DestinationDatabaseName OUData -DestinationServerName ocsql2014 -RunOutput
+    Copy-Table -SourceTableName SOURCE -DestinationTableName DEST -DestinationDatabaseName TestData -DestinationServerName sql2014 -RunOutput
         - Create new table but do not overwrite, copy data over,  execute on SQL server
 .EXAMPLE
-    Copy-Table -SourceTableName GZRADUS -DestinationTableName TEMPTABLE -OutputFile "$(Get-date -f 'MMddyy')_update.sql" -OverwriteExisting -RunOutput
+    Copy-Table -SourceTableName SOURCE -DestinationTableName DEST -OutputFile "$(Get-date -f 'MMddyy')_update.sql" -OverwriteExisting -RunOutput
         - Create or update table, copy and overwrite data, create file of the day to be stored, run output.
 .EXAMPLE
-    Copy-Table -SourceTableName GZRADUS -DestinationTableName TEMPTABLE -RowLimit 50 -RunOutput
+    Copy-Table -SourceTableName SOURCE -DestinationTableName DEST -RowLimit 50 -RunOutput
         - Create table, limit data to 50 rows, run output
 
 #>
