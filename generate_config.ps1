2d38892813c1edec3c4b4850f30b6f73caacf3af
Lawrance <hlawrance@otterbein.edu>
2018-08-24 15:23:32 -0400

    improved generate_config.ps1

--- generate_config.ps1
+++ generate_config.ps1
@@ -1,5 +1,18 @@
+if(-not(Test-Path .\private)){
+    Write-Verbose "Creating .\private diretory"
+    New-Item -Path .\private -ItemType directory
+}
 
-$Oracle_Credential_Path = ".\private\oracred.xml"
+if(Test-Path .\private\oracred.xml) {
+    $overwrite = switch (Read-Host -Prompt "oracred.xml exists. Create new? [Y/N]") {
+        { $_ -match 'Y|y' } { $true }
+        default {$false}
+    }
+}
+if($overwrite -or -not(Test-Path .\private\oracred.xml)) {
+    Write-Verbose "Generating fresh oracred.xml file. Please enter Oracle Credentials"
+    $Oracle_Credential_Path = ".\private\oracred.xml"
 
-# Oracle Credential File
-Get-Credential | Export-Clixml -Path $Oracle_Credential_Path
+    # Oracle Credential File
+    Get-Credential | Export-Clixml -Path $Oracle_Credential_Path
+}
